09cd80e1439feff8b0fc272dd3441facccdac28e
Codacy Archive <codacy-archive@local>
2025-11-27 11:39:20 +0100

    snapshot 2025-11-27T10:39:20.346454+00:00

--- src/mongo/db/query/compiler/optimizer/join/reorder_joins.cpp
+++ src/mongo/db/query/compiler/optimizer/join/reorder_joins.cpp
@@ -406,9 +406,12 @@ ReorderedJoinSolution constructSolutionWithRandomOrder(
                     *mca.lookupCollection(currentNode.collectionName)->getIndexCatalog(),
                     indexedJoinPreds);
                 indexEntry.has_value()) {
-                rhs = std::make_unique<FetchNode>(std::make_unique<IndexProbeNode>(
-                    currentNode.collectionName, std::move(indexEntry.value())));
-                // TODO SERVER-111910: Write an end-to-end test exercising this codepath.
+                rhs = std::make_unique<FetchNode>(
+                    std::make_unique<IndexProbeNode>(currentNode.collectionName,
+                                                     std::move(indexEntry.value())),
+                    currentNode.collectionName);
+                // TODO SERVER-111222: Write an end-to-end test exercising this codepath, once we
+                // can lower INLJ nodes to SBE.
                 if (auto matchExpr = currentNode.accessPath->getPrimaryMatchExpression();
                     matchExpr != nullptr && !matchExpr->isTriviallyTrue()) {
                     rhs->filter = matchExpr->clone();
